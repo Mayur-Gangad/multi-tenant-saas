207977bfa18e96475ca06b5ac0fad9137313ccdd
Mayur-Gangad <gangadmayur@gmail.com>
2026-02-22 12:53:37 +0530

    Project setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+import cors from "cors";
+
+const PORT = 3030;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+
+export default app;
